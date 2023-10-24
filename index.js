5d76df4a67c10027f4ad4ae0d51a063f1ab95849
cmd430 <cmd430@gmail.com>
2023-10-24 17:40:30 +1300

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-import fp from 'fastify-plugin';
-import Database from 'better-sqlite3';
+import fp from 'fastify-plugin'
 
-const fastifyBetterSqlite3 = (fastify, db, next) => {    
-    if (fastify.betterSqlite3) {
-        next(new Error('plugin already registered'));
-    }
-    
-    fastify.decorate('betterSqlite3', db);
-    fastify.addHook('onClose', (fastify, done) => db.close(done));
+const fastifyBetterSqlite3 = (fastify, db, next) => {
+  if (fastify.betterSqlite3) {
+    next(new Error('plugin already registered'))
+  }
 
-    next();
+  fastify.decorate('betterSqlite3', db)
+  fastify.addHook('onClose', (fastify, done) => db.close(done))
+
+  next()
 }
 
 export default fp(fastifyBetterSqlite3, {
-    fastify: '4.x',
-    name: 'fastify-better-sqlite3'
+  fastify: '4.x',
+  name: 'fastify-better-sqlite3'
 })
